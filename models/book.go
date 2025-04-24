0eb2e356bb8d66aab89cef173ea5c4f971b33f0e
Ishmael Akaboa <ishmaelakaboa@gmail.com>
2025-04-24 08:56:47 +0000

    Created Model

--- models/book.go
+++ models/book.go
@@ -1,2 +1,14 @@
 package models
 
+import "gorm.io/gorm"
+
+type Book struct {
+	gorm.Model
+	Title  string `json:"title" gorm:"not null"`
+	Author string `json:"author" gorm:"not null"`
+	Year   int    `json:"year"`
+}
+
+func (Book) TableName() string{
+	return "books"
+}
